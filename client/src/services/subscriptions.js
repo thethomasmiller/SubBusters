b3029f82faa7ad9493e1b6456967c1af5a3a8f1a
kellyappiah <kellybappiah@gmail.com>
2020-07-17 14:46:40 -0400

    apConfig

---
+++ client/src/services/subscriptions.js
@@ -0,0 +1,46 @@
+import api from './apiConfig'
+
+export const getSubscriptions = async () => {
+  try {
+    const response = await api.get('/subscriptions')
+    return response.data
+  } catch (error) {
+    throw erros
+  }
+}
+
+export const getSubscription = async id => {
+  try {
+    const response = await api.get(`/subscriptions/${id}`)
+    return response.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const createSubscription = async subscription => {
+  try {
+    const response = await api.post('//subscriptions', subscription)
+    return response.data 
+  } catch (error) {
+    throw error
+  }
+}
+
+export const updateSubscription = async(id, subscription)=> {
+  try {
+    const response = await api.put(`/subscriptions/${id}`, subscription)
+    return response.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const deleteSubscription = async id => {
+  try {
+    const response = await api.delete(`/subscriptions/${id}`)
+    return response.data
+  } catch (error) {
+    throw error
+  }
+}
