db7f6b0f655925a0f2f95344eb9fb8d45d254b90
kellyappiah <kellybappiah@gmail.com>
2020-07-20 16:21:11 -0400

    feature/class-names

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import './App.css'
-import Login from './screens/Login/Login'
-import SubscriptionCreate from './screens/SubscriptionCreate/SubscriptionCreate'
+// import Login from './screens/Login/Login'
+// import SubscriptionCreate from './screens/SubscriptionCreate/SubscriptionCreate'
 import SubscriptionDelete from './screens/SubscriptionDelete/SubscriptionDelete'
-import SubscriptionUpdate from './screens/SubscriptionUpdate/SubscriptionUpdate'
-import Subscriptions from './screens/Subscriptions/Subscriptions'
+// import SubscriptionUpdate from './screens/SubscriptionUpdate/SubscriptionUpdate'
+// import Subscriptions from './screens/Subscriptions/Subscriptions'
 import { Route, Switch } from 'react-router-dom'
 
 const App = () => {
   return (<>
     <div>
       <Switch>
-        <Route exact path='/' component={Login}/> 
-        <Route exact path='/subscriptions' component={Subscriptions}/>
-        <Route path='/add-subscription' component={SubscriptionCreate}/>
-        <Route exact path='/subscriptions/:id/update' component={SubscriptionUpdate}/>
+        {/* <Route exact path='/' component={Login}/>  */}
+        {/* <Route exact path='/subscriptions' component={Subscriptions}/> */}
+        {/* <Route path='/add-subscription' component={SubscriptionCreate}/> */}
+        {/* <Route exact path='/subscriptions/:id/update' component={SubscriptionUpdate}/> */}
         <Route exact path='/subscriptions/:id' component={SubscriptionDelete}/>
       </Switch>
     </div>
