83a4a0ea23fd93adba466d42f8e4da0f755b4385
kellyappiah <kellybappiah@gmail.com>
2020-07-21 14:39:07 -0400

    feature/filters

--- client/src/screens/Subscriptions/Subscriptions.jsx
+++ client/src/screens/Subscriptions/Subscriptions.jsx
@@ -7,8 +7,6 @@ import {getSubscriptions} from '../../services/subscriptions'
 
 
 
-
-
 class Subscriptions extends Component {
   constructor() {
     super()
@@ -25,10 +23,28 @@ class Subscriptions extends Component {
   }
  
   render() {
-    const subscriptionList = this.state.allSubscriptions.map((subscription, index) =>
-      <SubscriptionInfo _id={subscription._id} logoURL={subscription.logoURL} name={subscription.charge} billingDate={subscription.lastUsed} amount={subscription.amount} key = {index} />
-    
+    const musicList = this.state.allSubscriptions.filter(sub => sub.category === 'Music').map((entertainment, index)=>
+      <SubscriptionInfo _id={entertainment._id} logoURL={entertainment.logoURL} name={entertainment.charge} billingDate={entertainment.lastUsed} amount={entertainment.amount} key={index} />
     )
+
+    const moviesList = this.state.allSubscriptions.filter(sub => sub.category === 'Movies and TV').map((entertainment, index)=>
+    <SubscriptionInfo _id={entertainment._id} logoURL={entertainment.logoURL} name={entertainment.charge} billingDate={entertainment.lastUsed} amount={entertainment.amount} key={index} />
+    )
+    
+    const softwareList = this.state.allSubscriptions.filter(sub => sub.category === 'Software').map((entertainment, index)=>
+    <SubscriptionInfo _id={entertainment._id} logoURL={entertainment.logoURL} name={entertainment.charge} billingDate={entertainment.lastUsed} amount={entertainment.amount} key={index} />
+    )
+    
+    const gamingList = this.state.allSubscriptions.filter(sub => sub.category === 'Gaming').map((entertainment, index)=>
+    <SubscriptionInfo _id={entertainment._id} logoURL={entertainment.logoURL} name={entertainment.charge} billingDate={entertainment.lastUsed} amount={entertainment.amount} key={index} />
+    )
+
+    const newsList = this.state.allSubscriptions.filter(sub => sub.category === 'News').map((entertainment, index)=>
+    <SubscriptionInfo _id={entertainment._id} logoURL={entertainment.logoURL} name={entertainment.charge} billingDate={entertainment.lastUsed} amount={entertainment.amount} key={index} />
+  )
+
+
+
     
   return (
     <Layout>
@@ -46,8 +62,21 @@ class Subscriptions extends Component {
             </span>
         </section>
 
+        
         <div>
-          {subscriptionList}
+          {moviesList}
+        </div>
+        <div>
+          {musicList}
+        </div>
+        <div>
+          {softwareList}
+        </div>
+        <div>
+          {gamingList}
+        </div>
+        <div>
+          {newsList}
         </div>
         
         
