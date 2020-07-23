6ed127db6a0aeb37f598342e6bab1534853cc0ee
kellyappiah <kellybappiah@gmail.com>
2020-07-23 13:04:43 -0400

    last dance

---
+++ client/src/screens/WelcomePage/WelcomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../../components/shared/Layout/Layout'
+
+class WelcomePage extends Component{
+  render() {
+    return (
+      <Layout>
+
+      </Layout>
+    )
+  }
+}
