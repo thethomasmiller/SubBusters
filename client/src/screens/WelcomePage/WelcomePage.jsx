ba4edf4d3bd59f25f0a79d7ddd746d75598579a0
kellyappiah <kellybappiah@gmail.com>
2020-07-23 13:59:46 -0400

    lastdance

--- client/src/screens/WelcomePage/WelcomePage.jsx
+++ client/src/screens/WelcomePage/WelcomePage.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import Layout from '../../components/shared/Layout/Layout'
+import profilePic from '../../assets/images/profilePic.png'
 
 class WelcomePage extends Component{
   render() {
     return (
       <Layout>
-
+ <div className="user-header">
+          <img src={`${profilePic}`} className='profile-image' />
+          <h3 className="user-greeting">Hello, Jamie</h3>
+        </div>
+        
       </Layout>
     )
   }
 }
+
+export default WelcomePage
