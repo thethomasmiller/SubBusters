c434ad254de3d7b6ac4fa99a7c2bd79029aeb749
Thomas Miller <thomasmiller@Thomass-MacBook-Air.local>
2020-07-24 10:37:56 -0400

    match update

--- client/src/screens/SubscriptionCreate/SubscriptionCreate.jsx
+++ client/src/screens/SubscriptionCreate/SubscriptionCreate.jsx
@@ -30,7 +30,7 @@ class SubscriptionCreate extends Component {
       subscription: {
         ...this.state.subscription,
         [name]: value,
-        logoURL:"https://res.cloudinary.com/alienora/image/upload/v1595446140/Subscription_logo_placeholder_on9w2v.png"
+        logoURL: "https://res.cloudinary.com/alienora/image/upload/v1595446140/Subscription_logo_placeholder_on9w2v.png"
       }
     })
   }
@@ -58,82 +58,79 @@ class SubscriptionCreate extends Component {
         <div className='body-create'>
           <h2 className='add-subscription-title'>Add Subscription Manually</h2>
           <form className='create-form' onSubmit={this.handleSubmit}>
-            <div className= 'input-container'>
-            <label for='input-name' className='create-label'>Subscription</label>
-            <input
-              className='input-name'
-              placeholder='Enter Name'
-              value={subscription.name}
-              name='name'
-              onChange={this.handleChange}
-            />
-            </div>
-            <div className= 'input-container'>
-            <label for='input-category' className='create-label'>Category</label>
-            <select name='category' className='create-dropdown' onChange={this.handleChange}  >
-              <option value=''>Select</option>
-              <option value='Music'>Music</option>
-              <option value='Movies and TV'>Movies & TV</option>
-              <option value='Software'>Software</option>
-              <option value='Gaming'>Gaming</option>
-              <option value='News'>News</option>
-            </select>
-            </div>
-            <div className= 'input-container'>
-            <label for='input-billing-cycle' className='create-label'>Billing Cycle</label>
-            <select name='billingCycle' className='create-dropdown' onChange={this.handleChange}  >
-              <option value=''>Select</option>
-              <option value='monthly'>Monthly</option>
-              <option value='quarterly'>Quarterly</option>
-              <option value='annually'>Annually</option>
-            </select>
-            </div>
-<div div className= 'input-container'>
-            <label for='input-auto-renew' className='create-label'>Auto-Renew</label>
-            <select name='autoRenew' className='create-dropdown' onChange={this.handleChange}  >
-              <option value=''>Select</option>
-              <option value='true'>Yes</option>
-              <option value='false'>No</option>
-            </select>
-            </div>
-
-            <div  className= 'input-container'>
-            <label for='input-amount' className='create-label'>Amount</label>
-            <input
-              className='input-amount'
-              placeholder='Amount'
-              value={subscription.amount}
-              name='amount'
-              onChange={this.handleChange}
-            />
-            </div>
-            
-           <div  className= 'input-container'>
-            <label for='input-name' className='create-label'>Billing Date</label>
-            <input
-              className='input-billing-date'
-              placeholder='MM/DD/YY'
-              value={subscription.billingDate}
-              name='billingDate'
-              autoFocus
-              onChange={this.handleChange}
-            />
-            </div> 
-              
             <div className='input-container'>
-            <label for='input-website-url'className='create-label'>Website</label>
-            <input
-              className='input-website-url'
-              placeholder='Enter address'
-              value={subscription.websiteURL}
-              name='websiteURL'
+              <label for='input-name' className='create-label'>Name</label>
+              <input
+                className='input-name'
+                placeholder='Enter Name'
+                value={subscription.name}
+                name='name'
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className='input-container'>
+              <label for='input-category' className='create-label'>Category</label>
+              <select name='category' className='create-dropdown' onChange={this.handleChange}  >
+                <option value=''>Select</option>
+                <option value='Music'>Music</option>
+                <option value='Movies and TV'>Movies & TV</option>
+                <option value='Software'>Software</option>
+                <option value='Gaming'>Gaming</option>
+                <option value='News'>News</option>
+              </select>
+            </div>
+            <div className='input-container'>
+              <label for='input-billing-cycle' className='create-label'>Billing Cycle</label>
+              <select name='billingCycle' className='create-dropdown' onChange={this.handleChange}  >
+                <option value=''>Select</option>
+                <option value='monthly'>Monthly</option>
+                <option value='quarterly'>Quarterly</option>
+                <option value='annually'>Annually</option>
+              </select>
+            </div>
+            <div className='input-container'>
+              <label for='input-auto-renew' className='create-label'>Auto-Renew</label>
+              <select name='autoRenew' className='create-dropdown' onChange={this.handleChange}  >
+                <option value=''>Select</option>
+                <option value='true'>Yes</option>
+                <option value='false'>No</option>
+              </select>
+            </div>
 
-              autoFocus
-              onChange={this.handleChange}
-            />
+            <div className='input-container'>
+              <label for='input-amount' className='create-label'>Amount</label>
+              <input
+                className='input-amount'
+                placeholder='Amount'
+                value={subscription.amount}
+                name='amount'
+                onChange={this.handleChange}
+              />
+            </div>
+
+            <div className='input-container'>
+              <label for='input-name' className='create-label'>Billing Date</label>
+              <input
+                className='input-billing-date'
+                placeholder='MM/DD/YY'
+                value={subscription.billingDate}
+                name='billingDate'
+                onChange={this.handleChange}
+              />
+            </div>
+
+            <div className='input-container'>
+              <label for='input-website-url' className='create-label'>Website</label>
+              <input
+                className='input-website-url'
+                placeholder='Enter address'
+                value={subscription.websiteURL}
+                name='websiteURL'
+                onChange={this.handleChange}
+              />
             </div>
             <div className='create-buttons'>
-              <Link to='/subscriptions' style={{ textDecoration: 'none' }}><button className="submit-button">Back</button></Link>   
+              <Link to='/subscriptions' style={{ textDecoration: 'none' }}><button className="submit-button">Back</button></Link>
               <button type='submit' className="submit-button">Add</button>
             </div>
           </form>
